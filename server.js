a0197a554525f9ca4ccf3a2731369f5c8d165dbd
dhanasp <dhanashrinp3009@gmail.com>
2018-03-13 19:53:43 +0530

    deployement on heroku
    Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app= require('./app.js').app;
-const PORT = 9090;
+const PORT = process.env.PORT||9090;
 
 let server = http.createServer(app);
 console.log(`listening to ${PORT}`);
